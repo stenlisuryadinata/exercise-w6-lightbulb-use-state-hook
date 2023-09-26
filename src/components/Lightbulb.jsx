9dcea693e5c03a60456bd0de311ff1c38b71d183
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-09-26 19:56:51 +0700

    Update lightbulb.jsx
    Update lightbulb.jsx

--- src/components/Lightbulb.jsx
+++ src/components/Lightbulb.jsx
@@ -1,15 +1,28 @@
+import React, { useState } from 'react';
+import '../styles.css'; // Make sure to specify the correct path to your CSS file
+
 export const Lightbulb = () => {
-  // Initialize the state for on/off
+  const [lightOn, setLightOn] = useState(false); // Initialize the state for on/off
 
   const toggleLight = () => {
     // Toggle the on/off state
-    console.log("Clicked");
+    setLightOn(!lightOn);
   };
+
+  // Define the CSS class based on the lightOn state
+  const bulbClass = `lightbulb ${lightOn ? 'on' : 'off'}`;
+
+
   return (
-    <div className="lightbulb-container">
-      <div className="lightbulb" onClick={toggleLight}>
-        <div className="bulb"></div>
+    <React.Fragment>
+      <div className="lightbulb-container">
+        <div className={bulbClass} onClick={toggleLight}>
+          <div className="bulb"></div>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
+
+
+
